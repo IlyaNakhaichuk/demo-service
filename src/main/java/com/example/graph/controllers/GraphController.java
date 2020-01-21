9e00dc300ab0c7a7cd1205dad014db2a98f8e6f6
Ilya Nakhaichuk <ilya.nakhaichuk@gmail.com>
2020-01-21 14:08:44 +0200

    Change /get

--- src/main/java/com/example/graph/controllers/GraphController.java
+++ src/main/java/com/example/graph/controllers/GraphController.java
@@ -28,7 +28,7 @@ public class GraphController {
 
     @GetMapping("/get")
     public String getMethod() {
-        return "Tehtelka, Privet";
+        return "Hello world!";
     }
 
     @GetMapping("/getRandom")
